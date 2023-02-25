b813d33aaed3f26b878baaad84c2434e1890a8f5
DevEmmy <eolaosebikan60@gmail.com>
2023-02-25 08:25:28 -0800

    added background music

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Banner from './Banner'
 import BottomBanner from './BottomBanner'
 import Branches from './Branches'
 import BriefAbout from './BriefAbout'
 import Footer from './Footer'
 import Nav from './Nav'
+import useSound from 'use-sound';
 
 const MainPage = () => {
+    const [play] = useSound("https://res.cloudinary.com/devemmy/video/upload/v1677340013/yt1s.com_-_Ayra_Starr_Rush_Official_Music_Video_lprrvz.mp3");
+
+    const [playing, setPlaying] = useState(false)
+
+    const startPlaying = ()=>{
+        if(!playing){
+            play()
+            console.log("play")
+            setPlaying(true)
+        }
+        // play()
+    }
+
   return (
-    <>
+    <div onClick={startPlaying} onTouchStart={startPlaying} onLoadStart={startPlaying}>
         <Nav />
         <Banner />
         <Branches />
@@ -16,7 +30,7 @@ const MainPage = () => {
         <BottomBanner />
 
         <Footer />
-    </>
+    </div>
   )
 }
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,24 @@
 import MetaTags from '@/Components/MetaTags'
 import MainPage from '@/Components/MainPage'
 
+import { Fragment, useEffect } from 'react';
+
 export default function Home() {
+  
+  // useEffect(()=>{
+  //   // play()
+  //   window.addEventListener("touch", ()=> {
+  //     play()
+  //     console.log("hey")
+  //   })
+    
+  // }, [])
+  
   return (
-    <>
+    <Fragment onScroll={()=> console.log("hey")}>
       <MetaTags />
       <MainPage />
-    </>
+      
+    </Fragment>
   )
 }
